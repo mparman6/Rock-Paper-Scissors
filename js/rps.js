276bd72a13d0099996acfa7511633f7f7e8ebad6
Matt <mparman6@yahoo.com>
2015-12-11 17:29:44 -0500

    Added function to randomly select computer 'weapon'

--- js/rps.js
+++ js/rps.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
+  var compPicks = ["Rock", "Paper", "Scissors"];
+
   $('#start').on('click', function() {
     $('.hidden').toggleClass("hidden");
-    $('.hidden').toggleClass("");
+    $('.hidden').toggleClass("off");
   });
+
+  $("").on("click", function() {
+    var randomPick = Math.floor(Math.random() * comPicks.length);
+    alert(randomPick);
+  });
+  
 });
