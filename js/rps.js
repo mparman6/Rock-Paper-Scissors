f53d5b36ef9a01eea599833c6c0178af233af637
Matt <mparman6@yahoo.com>
2015-12-12 12:31:19 -0500

    made all buttons functional and display choices

--- js/rps.js
+++ js/rps.js
@@ -1,14 +1,37 @@
 $(document).ready(function() {
   var compPicks = ["Rock", "Paper", "Scissors"];
+  var randomPick = Math.floor(Math.random() * compPicks.length);
 
   $('#start').on('click', function() {
     $('.hidden').toggleClass("hidden");
     $('.hidden').toggleClass("off");
   });
 
-  $(".weapon").on("click", function() {
+  $("#rock").on("click", function() {
     var randomPick = Math.floor(Math.random() * compPicks.length);
-    alert("it works");
+    $("#opp").fadeIn(250, function() {
+      $(this).html(compPicks[randomPick]);
+    });
+
+    $("#you").html("Rock");
+  });
+
+  $("#paper").on("click", function() {
+    var randomPick = Math.floor(Math.random() * compPicks.length);
+    $("#opp").fadeIn(250, function() {
+      $(this).html(compPicks[randomPick]);
+    });
+
+    $("#you").html("Paper");
+  });
+
+  $("#scissors").on("click", function() {
+    var randomPick = Math.floor(Math.random() * compPicks.length);
+    $("#opp").fadeIn(250, function() {
+      $(this).html(compPicks[randomPick]);
+    });
+
+    $("#you").html("Scissors");
   });
 
 });
